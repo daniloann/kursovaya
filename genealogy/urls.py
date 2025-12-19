3bc5e8c352da55db94951bcfee237a2e84bba459
Anna Danilova <danilovaanna607@gmail.com>
2025-12-20 01:07:16 +0300

    new

--- genealogy/urls.py
+++ genealogy/urls.py
@@ -6,16 +6,18 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # ваши API эндпоинты
+    path('api/', include('api.urls')),
 
     # Главная страница
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
 
-    # Для SPA (если есть клиентский роутинг)
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
+    # Для SPA - можно использовать path с угловыми скобками
+    path('tree/', TemplateView.as_view(template_name='index.html')),
+    path('search/', TemplateView.as_view(template_name='index.html')),
+
 ]
 
-# Только для development
+# Для статики в development
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
